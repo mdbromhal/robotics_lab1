f37eb0a690e190167fc0033fb0e6741932cbf59f
mdbromhal <mdalia@proton.me>
2024-01-31 19:51:55 +0000

    help from Andrew

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -11,6 +11,8 @@ from robotics_lab1.msg import Turtlecontrol
 # Declaring a global variable
 pos_msg = Turtlecontrol()
 
+pose_message = Pose()
+
 import math
 
 ROTATION_SCALE = 180.0/math.pi
@@ -19,13 +21,16 @@ def turtlesim_pose_callback(data):
 	'''
 	Function to retreive position date for the Turtlesim_node subscriber
 	'''
-
+	
+	global pose_message
+	
 	# Convert angular position to degrees
 	rot_in_degree = data.theta * ROTATION_SCALE
 	
 	# Convert x and y to cm
-	x_in_cm = data.x * 100
-	y_in_cm = data.y * 100
+	pose_message.x = data.x
+	pose_message.y = data.y
+	pose_message.theta = data.theta
 
 
 def turtlecontrol_pose_callback(data):
@@ -40,9 +45,10 @@ def turtlecontrol_pose_callback(data):
 	pos_msg.theta = data.theta * ROTATION_SCALE
 	
 	# Convert x and y to cm
-	pos_msg.x = data.x * 100
-	pos_msg.y = data.y * 100
-# WHICH ONE TO USE?
+	pos_msg.x = data.x
+	pos_msg.y = data.y
+	pos_msg.theta = data.theta
+
 
 def proportional_controller(data):
 	'''
@@ -50,12 +56,11 @@ def proportional_controller(data):
 	'''
 	# Equation is vt = Kp(xd - xt)
 	
-	# DON'T ENTIRELY UNDERSTAND WHY WE USE DATA
 	pos_msg.kp = data.kp
 	pos_msg.xd = data.xd
+	return vt
+	 
 	
-	vt = pos_msg.kp * (pos_msg.xd - pos_msg.x) # IS THIS RIGHT?
-
 if __name__ == '__main__':
 
 	# Initialize the node
@@ -64,20 +69,20 @@ if __name__ == '__main__':
 	# Add suscriber to receive position info from turtlesim_node
 	pos_sub1 = rospy.Subscriber('/turtle1/pose', Pose, turtlesim_pose_callback)
 	
-	pos_sub2 = rospy.Subscriber('/turtle1/control_params', Turtlecontrol, proportional_controller)
-	# DOES THIS CREATE A NEW TOPIC AUTOMATICALLY?
-	# HOW TO WORK WITH PROPORTIONAL CONTROLLER?
+	pos_sub2 = rospy.Subscriber('/turtle1/control_params', Turtlecontrol, turtlecontrol_pose_callback)
 	
 	# Add publisher with new topic using Shortpos message
 	pos_pub = rospy.Publisher('/turtle1/Turtlecontrol', Turtlecontrol, queue_size = 10)
+	
 	
 	# Set frequency for this loop - 10 Hz
 	loop_rate = rospy.Rate(10)
 	
 	while not rospy.is_shutdown():
+		vt = proportional_controller()
 		
 		# Publish the message
-		pos_pub.publish(pos_msg)
+		pos_pub.publish(vt)
 		
 		# Wait 0.1s until next loop and repeat
 		loop_rate.sleep()
