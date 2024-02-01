fec3357eca2dae436438f4ba1ed59a1f337e975f
mdbromhal <mdalia@proton.me>
2024-02-01 16:30:08 +0000

    updated comments

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -8,9 +8,10 @@ from turtlesim.msg import Pose
 # Read Turtlecontrol messages
 from robotics_lab1.msg import Turtlecontrol
 
-# Declaring a global variable
+# Declaring a global variable for the Turtlecontrol messages
 turtle_msg = Turtlecontrol()
 
+# Declaring a global variable for the Pose messages
 pose_msg = Pose()
 
 import math
@@ -24,13 +25,10 @@ def turtlesim_pose_callback(data):
 	
 	global pose_msg
 	
-	# Convert angular position to degrees
-	rot_in_degree = data.theta * ROTATION_SCALE
-	
 	# Convert x and y to cm
 	pose_msg.x = data.x
 	pose_msg.y = data.y
-	pose_msg.theta = data.theta
+	pose_msg.theta = data.theta * ROTATION_SCALE
 
 
 def turtlecontrol_pose_callback(data):
@@ -42,19 +40,17 @@ def turtlecontrol_pose_callback(data):
 	global turtle_msg
 	
 	# Convert x and y to cm
-	turtle_msg.x = data.x
-	turtle_msg.y = data.y
-	turtle_msg.theta = data.theta
+	turtle_msg.kp = data.kp
+	turtle_msg.xd = data.xd
 
 
 def proportional_controller(data):
 	'''
 	This controller generates a control input (velocity of Turtlebot) to minimize error bw the desired output (target position for Turtlebot) and current output (current position of Turtlebot)
 	'''
-	# Equation is vt = Kp(xd - xt)
 	
-	pos_msg.kp = data.kp
-	pos_msg.xd = data.xd
+	# Equation is vt = Kp(xd - xt)
+	vt = turtle_msg.kp * (turtle_msg.xd - pose_msg.x)
 	
 	return vt
 	 
@@ -67,6 +63,7 @@ if __name__ == '__main__':
 	# Add suscriber to receive position info from turtlesim_node
 	pos_sub1 = rospy.Subscriber('/turtle1/pose', Pose, turtlesim_pose_callback)
 	
+	# Second subscriber, subscribed to Tur
 	pos_sub2 = rospy.Subscriber('/turtle1/control_params', Turtlecontrol, turtlecontrol_pose_callback)
 	
 	# Add publisher with new topic using Shortpos message
@@ -77,6 +74,7 @@ if __name__ == '__main__':
 	loop_rate = rospy.Rate(10)
 	
 	while not rospy.is_shutdown():
+	
 		vt = proportional_controller()
 		
 		# Publish the message
